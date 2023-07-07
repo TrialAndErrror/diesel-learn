2e58e6b3e365a27892c98066733d0159515a0e36
Wade Green <thewadegreen@gmail.com>
2023-07-07 12:04:00 -0400

    working

--- src/models.rs
+++ src/models.rs
@@ -3,15 +3,17 @@ use diesel::prelude::*;
 use serde::Serialize;
 
 #[derive(Insertable)]
-#[table_name = "todos"]
-pub struct NewTodo {
-    pub text: String,
+#[table_name = "groceries"]
+pub struct NewGrocery {
+    pub name: String,
+    pub amount: String,
 }
 
 #[derive(Debug, Queryable, Serialize)]
 pub struct Todo {
     pub id: i32,
-    pub text: String,
+    pub name: String,
+    pub amount: String,
     pub done: bool,
     pub finish_timestamp: Option<chrono::DateTime<chrono::Utc>>,
     pub timestamp: chrono::DateTime<chrono::Utc>,
